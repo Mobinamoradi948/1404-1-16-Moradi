301ad50499452fac72e59869029f8323f43bc237
Mobinamoradi948 <mobi.88777@gmail.com>
2025-04-04 22:08:40 +0330

    Add files via upload

---
+++ Score.py
@@ -0,0 +1,10 @@
+# Grade Converter
+grade = float(input("Enter grade: "))
+if grade > 90:
+    print("Excellent")
+elif grade > 70:
+    print("Good")
+elif grade > 50:
+    print("Average")
+else:
+    print("Fail")
---
+++ max.py
@@ -0,0 +1,9 @@
+... num1 = float(input("Enter the first number: "))
+... num2 = float(input("Enter the second number: "))
+... num3 = float(input("Enter the third number: "))
+... num4 = float(input("Enter the fourth number: "))
+... 
+... # Find the largest number
+... max_num = max(num1, num2, num3, num4)
+... 
+... # Display the largest number
